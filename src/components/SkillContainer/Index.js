c0808b92c3c958fb0be62c967dd31550e40c3fb4
Conrad Jessup <neil.jessup@gmail.com>
2019-11-21 12:55:37 -0600

    registration component built and rendering, but needs the skill ocomponent

---
+++ src/components/SkillContainer/Index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SkillContainer extends React.Component{
+
+  render(){
+    return (
+      <div> SkillContainer </div>
+    );
+  }
+}
+export default SkillContainer;
