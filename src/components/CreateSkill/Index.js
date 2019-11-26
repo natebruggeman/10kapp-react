164d4a82dcf10afa75cca8067e776e05bbec2889
Conrad Jessup <neil.jessup@gmail.com>
2019-11-25 22:04:08 -0600

    create modal as main component, without functional component

--- src/components/CreateSkill/Index.js
+++ src/components/CreateSkill/Index.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import CreateSkillModal from '../CreateSkillModal/Index.js';
-import { Icon } from 'semantic-ui-react';
+import { Icon,  Form, Button, Label, Header, Modal } from 'semantic-ui-react';
 
 class CreateSkill extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
 
     this.state = {
       goal: '',
@@ -13,7 +12,7 @@ class CreateSkill extends Component {
     }
   }
   handleChange = (e) => {
-
+    e.preventDefault();
     this.setState({[e.currentTarget.name]: e.currentTarget.value})
   }
   render(){
@@ -24,13 +23,37 @@ class CreateSkill extends Component {
         name='plus'
         onClick={this.props.openModal}
         />
-
-        <CreateSkillModal
-          addSkill={this.props.addSkill}
-          open={this.props.open}
-          close={this.props.closeModal}
-          handleCreateChange={this.handleCreateChange}
-        />
+        <Modal open={this.props.open} closeIcon onClose={this.props.close}>
+          <Header>Create Skill</Header>
+          <Modal.Content>
+            <Form onSubmit={(e) => this.state.addSkill(e, this.state.goal)}>
+              <Label> Goal: </Label>
+              <Form.Input
+                type="text"
+                name="goal"
+                value={this.state.goal}
+                onChange={this.state.handleCreateChange}
+              />
+              <Label> Objective: </Label>
+              <Form.Input
+                type="text"
+                name="objective"
+                value={this.state.objective}
+                onChange={this.props.handleCreateChange}
+              />
+              <Label> Time: </Label>
+              <Form.Input
+                type="number"
+                name="time"
+                value={this.props.time}
+                onChange={this.props.handleCreateChange}
+              />
+              <Modal.Actions>
+                <Button color='green' type="submit">Create Skill</Button>
+              </Modal.Actions>
+            </Form>
+          </Modal.Content>
+        </Modal>
       </React.Fragment>
       )
   }
